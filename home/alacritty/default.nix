620085f8de8b5e8967fb9a3179beb57663463582
nino <ninolt>
2025-05-20 17:21:01 +0200

    feat: Adding padding to alacritty and changing font size

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -5,6 +5,17 @@
       general = {
         import = [ "/home/nino/.config/nixos/home/alacritty/gruvbox_dark.toml" ];
       };
+
+      font = {
+        size = 14.00;
+      };
+
+      window = {
+        padding = {
+          x = 20;
+          y = 20;
+        };
+      };
     };
   };
 }
